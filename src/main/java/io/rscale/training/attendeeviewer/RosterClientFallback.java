5fc2bc2709d6a362303dd51ad86967349e93766b
Gerard Mulder <g.mulder@cgi.com>
2017-06-08 09:10:46 -0600

    Initial version

---
+++ src/main/java/io/rscale/training/attendeeviewer/RosterClientFallback.java
@@ -0,0 +1,25 @@
+package io.rscale.training.attendeeviewer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RosterClientFallback implements RosterClient {
+
+	@Override
+	public PagedResources<Attendee> getAttendees(int page) {
+		List<Attendee> attendees = new ArrayList<>();
+		attendees.add(new Attendee());
+		return new PagedResources<>(attendees, new PagedResources.PageMetadata(1, 1, 1, 1), new Link("/"));
+	}
+
+	@Override
+	public Attendee getAttendee(String attendeeId) {
+		return new Attendee();
+	}
+
+}
